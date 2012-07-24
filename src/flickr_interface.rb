055daaf04e58e6cb3899e6e45e285fd1ef6d5409
= <martin.woz@gmail.com>
2012-07-23 22:05:02 -0500

    Fleshed out date iteration, user parameters

--- src/flickr_interface.rb
+++ src/flickr_interface.rb
@@ -1,23 +1,3 @@
-#!/usr/bin/ruby
-# Wrapper for getting the URLs of images on flickr
+require "bundler/setup"
+require "hpricot"
 
-class FlickrInterface 
-	def initialize
-		@base_url = 'http://www.flickr.com/explore/interesting'
-	end
-
-	def get_calendar_url year=nil, month=nil 
-		t = Time.now
-		year ||= t.year
-		month ||= t.month
-
-		# Pad dates
-		[@base_url, "%04d" % year, "%02d" % month]
-				.join('/')
-	end
-
-end
-
-f = FlickrInterface.new
-puts f.get_calendar_url
-puts f.get_calendar_url 1, 1
